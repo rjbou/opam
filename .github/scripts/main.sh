336127738f42109d34cd1f5b67b9e35ef1385e17
Raja Boujbel <raja.boujbel@ocamlpro.com>
2021-10-25 12:00:14 +0200

    rm

--- .github/scripts/main.sh
+++ .github/scripts/main.sh
@@ -19,6 +19,7 @@ export OCAMLRUNPARAM=b
 
   ./configure --prefix ~/local --with-mccs
   dune --version
+  dune build --help=plain | grep promote-install
   dune build --help=plain 2>/dev/null | sed -ne 's/^[[:space:]]*\(--promote-install-files\)\(\[.*\|[[:space:]]\)*$/ \1/p'
   exit 3
   if [[ $OPAM_TEST$OPAM_COLD -eq 0 ]] ; then
