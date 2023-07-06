0be9fe990d9e9979fec989e66c15a8f5e37efdfe
Raja Boujbel <raja.boujbel@ocamlpro.com>
2023-07-06 14:44:34 +0200

    rm

--- .github/scripts/main/main.sh
+++ .github/scripts/main/main.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -xue
+set -xe
 
 
 if [ "$OPAM_DOC" = "1" ]; then
@@ -9,6 +9,8 @@ if [ "$OPAM_DOC" = "1" ]; then
     . .github/scripts/common/hygiene-preamble.sh
     set -x
     diff="git diff $BASE_REF_SHA..$PR_REF_SHA"
+    $diff --name-only --diff-filter=A
+    $diff --name-only --diff-filter=A | grep 'src/.*mli'
     files="`$diff --name-only --diff-filter=A | grep 'src/.*mli'`"
     if [ -n "$files" ]; then
       echo '::group::new module added - checking it'
