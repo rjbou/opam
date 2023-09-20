e2ffc3a7b075edbc422b231764d8e45af0d1e986
Raja Boujbel <raja.boujbel@ocamlpro.com>
2023-09-20 18:51:15 +0200

    remove debug info

--- src/core/opamSystem.ml
+++ src/core/opamSystem.ml
@@ -488,60 +488,13 @@ let t_resolve_command =
         name ^ ".exe"
       else name
     in
-    let seps = String.map (function '/' -> '|' | '\\' -> '!' | c -> c) in
-(*
-    let _dirs =
-      let pwd = Sys.getcwd () in
-      let sel rec_ =
-        List.filter (fun s ->
-            not ((OpamStd.String.contains ~sub:"OPAM" s) || (OpamStd.String.contains ~sub:"REPO" s)))
-          (rec_ pwd)
-      in
-      let aux rec_ =
-        List.map (fun s -> (seps s), Sys.file_exists s)
-        (sel rec_)
-      in
-      let item rec_ = OpamStd.Format.itemize (fun (p,x) -> Printf.sprintf "%s -- %B" p x) (aux rec_) in
-      OpamConsole.error "------\n%s\n---\n%s\n---\n%s\n-------"
-        pwd
-        (item rec_dirs)
-        (item rec_files);
-      sel rec_dirs
-    in
-*)
-(*
-    let _check_path =
-      match List.find_opt (fun s -> OpamStd.String.contains ~sub:"opam-reftest" s) path, dirs with
-      | Some path, [dir] ->
-        OpamConsole.error "found %s" (seps path);
-        OpamConsole.error "is it the same %B lentgth %B" (String.equal path dir) (String.length path = String.length dir);
-        if not (String.equal path dir) then
-          (for i = 0 to (max (String.length dir) (String.length path)) do
-             let p = try path.[i] with Invalid_argument _ -> 'X' in
-             let d = try dir.[i] with Invalid_argument _ -> 'X' in
-             OpamConsole.note "'%c' - '%c' - %B" p d (Char.equal p d)
-           done)
-      | _, _ -> OpamConsole.note "not found"
-    in
-*)
     let possibles =
-      OpamConsole.error "looking for %s" name;
       OpamStd.List.filter_map (fun path ->
           let candidate = Filename.concat path name in
-          OpamConsole.warning "candidate file ? %B directory ? %B -- path %s exists ? %B"
-            (Sys.file_exists candidate)
-            (try Sys.is_directory candidate with Sys_error _ -> false)
-            (seps path)
-            (Sys.file_exists path);
           if Sys.file_exists candidate && not (Sys.is_directory candidate) then
             Some candidate else None)
         path
     in
-    OpamConsole.error "possibles %s"
-      (OpamStd.List.to_string (fun x ->
-           Printf.sprintf "%s:%B"
-             (String.map (function '/' -> '|' | '\\' -> '!' | c -> c) x) (check_perms x))
-          possibles);
     match List.find check_perms possibles with
     | cmdname -> `Cmd cmdname
     | exception Not_found ->
