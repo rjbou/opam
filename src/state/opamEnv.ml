24f9fb01c3f7bf8954310b560caf70156ae2e0a9
David Allsopp <david.allsopp@metastack.com>
2024-05-02 10:56:59 +0100

    Fix incorrect reverting of =+ and =:

--- src/state/opamEnv.ml
+++ src/state/opamEnv.ml
@@ -401,7 +401,7 @@ let reverse_env_update ~sepfmt var op arg cur_value =
   | EqPlus | Cygwin ->
     (match unzip_to ~sepfmt var arg (List.rev cur_value) with
      | None -> None
-     | Some (rl1, l2) -> Some (List.rev l2, List.rev rl1))
+     | Some (rl1, l2) -> Some (l2, List.rev rl1))
   | ColonEq ->
     (match unzip_to var ~sepfmt arg cur_value with
      | Some ([], [{ tr_orig = ""; _}]) -> Some ([], [])
@@ -409,7 +409,7 @@ let reverse_env_update ~sepfmt var op arg cur_value =
   | EqColon ->
     (match unzip_to ~sepfmt var arg (List.rev cur_value) with
      | Some ([], [{ tr_orig = ""; _}]) -> Some ([], [])
-     | Some (rl1, l2) -> Some (List.rev l2, List.rev rl1)
+     | Some (rl1, l2) -> Some (l2, List.rev rl1)
      | None -> None)
 
 let map_update_names env_keys updates =
