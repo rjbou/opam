cf8abd6249ab47b84d724bfe38bb491beeb32d1c
Raja Boujbel <raja.boujbel@ocamlpro.com>
2026-03-26 16:00:35 +0100

    #6871 backport continuing

--- src/repository/opamRepositoryPath.ml
+++ src/repository/opamRepositoryPath.ml
@@ -55,18 +55,28 @@ let files repo_root prefix nv =
   packages repo_root prefix nv / "files"
 
 let install_nv_dir filename =
+  let rec find_files prefix_files tail =
+    match tail with
+    | "files"::_ -> Some prefix_files
+    | h::t -> find_files (h::prefix_files) t
+    | [] -> None
+  in
   let rec aux (pre, rest) =
     match rest with
-    | "packages"::n::nv::"files"::_ ->
-      (* We don't check that n = name nv because historically opam permit to
-         have an opam file that is not strictly packages/name/name.version *)
-      (match OpamPackage.of_string_opt nv with
-       | Some p ->
-         let dir =
-           OpamFilename.Dir.of_list (List.rev pre @ "packages"::n::nv::[])
-         in
-         Some (p, dir)
-       | None -> None)
+    | "packages"::packages ->
+      (* We don't check packages/name/name.version layer because repo loading
+         is more permissive *)
+      (match find_files [] packages with
+       | Some (nv::prefix_files) ->
+         (match OpamPackage.of_string_opt nv with
+          | Some pkg ->
+            let dir =
+              OpamFilename.Dir.of_list
+                (List.rev (nv :: prefix_files @ ("packages"::pre)))
+            in
+            Some (pkg, dir)
+          | None -> None)
+       | None | Some [] -> None)
     | p::r -> aux (p::pre, r)
     | [] -> None
   in
