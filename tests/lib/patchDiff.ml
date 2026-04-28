17ebce7cebf106f21ff405b30dfec6b01632458c
Raja Boujbel <raja.boujbel@ocamlpro.com>
2026-04-28 13:08:56 +0200

    patchdiff: add dir-file transformations tests

--- tests/lib/patchDiff.ml
+++ tests/lib/patchDiff.ml
@@ -97,6 +97,36 @@ let content_file_dir = [
   };
 ]
 
+let content_file_dir_with_content = [
+  same_file;
+  { name = "dir-fst-file-snd";
+    first = Dir [ "fst", foo; "remove-me", bar ];
+    second = File foo;
+  };
+]
+
+let content_file_dir_with_content_error = [
+  same_file;
+  { name = "dir-fst-file-snd";
+    first = Dir [ "fst", foo; "remove-me", bar; "i-wont-be-removed", "baz"];
+    second = File foo;
+  };
+]
+
+let gitdiff_patch_failure_dir_non_empty = {|
+diff --git b/dir-fst-file-snd/fst a/dir-fst-file-snd
+similarity index 100%
+rename from dir-fst-file-snd/fst
+rename to dir-fst-file-snd
+diff --git b/dir-fst-file-snd/remove-me a/dir-fst-file-snd/remove-me
+deleted file mode c0ffee
+index c0ffee..c0ffee
+--- b/dir-fst-file-snd/remove-me
++++ /dev/null
+@@ -1 +0,0 @@
+-bar
+|}
+
 let content_symlink_fst = [
   same_file;
   { name = "linked-file-fst";
@@ -468,6 +498,16 @@ let tests = [
     content = content_single_file_in_dir_snd;
     kind = DiffPatch;
     git = true;
+  };
+  { label = "diff dir/file error, with content in the dir that is removed";
+    content = content_file_dir_with_content;
+    kind = DiffPatch;
+    git = true;
+  };
+  { label = "diff dir/file error, with content in the dir that is not removed";
+    content = content_file_dir_with_content_error;
+    kind = Patch gitdiff_patch_failure_dir_non_empty;
+    git = false;
   };
 ]
 
