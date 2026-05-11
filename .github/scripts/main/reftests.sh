10f42f06f61d58b03479c563e0a8e57193bb8349
Raja Boujbel <raja.boujbel@ocamlpro.com>
2026-05-11 16:27:41 +0200

    add micro_httpd in reftest-ci

--- .github/scripts/main/reftests.sh
+++ .github/scripts/main/reftests.sh
@@ -20,4 +20,11 @@ esac
 
 export PATH="$PREFIX/bin:$PATH"
 
+git clone https://codeberg.org/kit-ty-kate/micro_httpd
+pushd micro_httpd
+dune build -p micro_httpd
+dune install --root . --prefix "$CONFIGURE_PREFIX"
+popd
+rm -rf micro_httpd
+
 make tests
